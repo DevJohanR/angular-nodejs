7b02ddc21a541678d4bb75caf6d6131ba3d41750
DevJohanR <desarrollador7777@gmail.com>
2024-10-07 12:44:43 -0500

    cliente preparado para despliegue

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  endpoint: 'http://localhost:3000/'
+  production: true,
+  endpoint: 'https://angular-nodejs-x3zl.onrender.com'
 };
 
 /*
